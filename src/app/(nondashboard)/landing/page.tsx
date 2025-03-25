fd9336e40b2c0a33dd3da20ebeb393af36c866de
shohan-pherones <shohan.sub.56@gmail.com>
2025-03-25 18:05:57 +0600

    add discover section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,3 +1,4 @@
+import DiscoverSection from "./DiscoverSection";
 import FeaturesSection from "./FeaturesSection";
 import HeroSection from "./HeroSection";
 
@@ -6,6 +7,7 @@ const Landing = () => {
     <div>
       <HeroSection />
       <FeaturesSection />
+      <DiscoverSection />
     </div>
   );
 };
